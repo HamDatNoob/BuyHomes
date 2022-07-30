273eecc5df5d0404a77a3fd5b7c0f4a560ba68ca
HamDatNoob <hamthenoob@gmail.com>
2022-07-30 02:29:53 -0400

    added renaming

--- src/main/java/dev/hamsjunk/buyhomes/misc/TakenIndexes.java
+++
@@ -1,7 +0,0 @@
-package dev.hamsjunk.buyhomes.misc;
-
-public class TakenIndexes {
-    public static Integer[] get() {
-        return new Integer[]{ 7, 8, 16, 17, 25, 26, 34, 35, 43, 44, 52, 53 };
-    }
-}
